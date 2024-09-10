1860c8a4f716499da4322db4f9400709e06dfd8a
SrinidDev <45865576+SrinidDev@users.noreply.github.com>
2024-09-10 13:46:27 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Medical Chat Bot',
+    version='0.1',
+    packages=find_packages(),
+    author='Srinidhi',
+    author_email='srinidhidevaraj12@gmail.com'
+
+)
